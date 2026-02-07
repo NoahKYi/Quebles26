d7afafe3566de1abb01a2027ddc218c38f9d783d
NoahKYi <yinoah8@gmail.com>
2026-02-06 21:27:50 -0700

    i dont know what this is

--- game/control.gd
+++ game/control.gd
@@ -1,26 +1,32 @@
 extends Control
 
-func _ready():
-	Reset_Timer()
-	pass # Replace with function body.
-
 var sec = 0
 var min = 0
-var defsec = 30 # Default Seconds
-var defmin = 1 # Default Minutes
+var defsec = 0 # Default Seconds
+var defmin = 5 # Default Minutes
 
 
-func _on_timeout():
-	if sec ==0:
-		if min > 0:
-			min -= 1
-			sec = 60		#Pass the Timer
-	sec-=1
-
-	$Label.text = String(sec)+":"+String(min)
-	print(String(sec)+":"+String(min))
-	pass # Replace with function body.
+func _ready():
+	Reset_Timer()
+	_on_timeout()
 	
 func Reset_Timer():
 	sec = defsec
 	min = defmin
+	
+func _on_timeout():
+	if min == 0 and sec == 0:
+		print("Game Over")
+		$Timer.stop()
+		return
+		
+	if sec == 0:
+		min -= 1
+		sec = 59
+		return
+		
+	else:
+		sec -= 1
+		return
+		
+	$Label.text = "%02d:%02d" % [min, sec]
